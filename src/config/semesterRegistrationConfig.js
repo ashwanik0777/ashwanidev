e33804927c0c0a7cf07baf217fd1a27e2f850563
Ashwani Kushwaha <ashwanik346981@gmail.com>
2026-07-31 10:03:37 +0530

    update files

--- src/config/semesterRegistrationConfig.js
+++
@@ -1,5 +0,0 @@
-const trimSlash = (v) => String(v || '').replace(/\/+$/, '');
-
-export const semesterAuthApiUrl = trimSlash(import.meta.env.VITE_SEMESTER_AUTH_API_URL || '');
-export const semesterDataApiUrl = trimSlash(import.meta.env.VITE_SEMESTER_DATA_API_URL || '');
-export const isMockMode = !semesterAuthApiUrl && !semesterDataApiUrl;

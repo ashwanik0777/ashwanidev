c275596cd9b5f29259eddf7567ab195e94e2164b
decodermayank <mayankraj1034@gmail.com>
2026-08-19 14:15:25 +0530

    Hide Placement section from navigation bar for School of Humanities & Social Sciences (SOHSS)

--- src/components/departments/Navbar.jsx
+++ src/components/departments/Navbar.jsx
@@ -555,7 +555,7 @@ const Navbar = () => {
             </AnimatePresence>
           </motion.li>
         ))}
-        {currentSchoolCode !== "SOE" && currentSchoolCode !== "SOBSC" && currentSchoolCode !== "SOM" && (
+        {currentSchoolCode !== "SOE" && currentSchoolCode !== "SOBSC" && currentSchoolCode !== "SOM" && currentSchoolCode !== "SOHSS" && (
           <motion.li whileHover={{ y: -2 }} transition={{ duration: 0.2 }}>
             <Link
               to={routes.placement}
